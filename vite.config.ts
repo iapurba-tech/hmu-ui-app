b7465227aafed98e7c7b35f749cf9244df6c2221
Apurba Panja <apurbapanja1@gmail.com>
2026-03-25 11:09:04 +0530

    #1 added local url proxy for api calls

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   server: {
     port: 3000, 
     strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
   },
   test: {
     globals: true,
